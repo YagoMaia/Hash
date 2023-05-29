4d52015270e26ce8c2c167fd743a61020a6ad9b4
YagoMaia <yagohenriquev123@gmail.com>
2023-05-29 16:30:45 -0300

    modified:   manipulacao.c 	renamed:    brascubas.txt -> output/brascubas.txt 	new file:   output/manipulacao.exe 	new file:   output/teste.txt

--- manipulacao.c
+++ manipulacao.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-
+int main(void){
+    int num = 0;
     FILE *file;
+    file = fopen("teste.txt", "r");
+    // fprintf(file, "Algo depois do hello world."); //Vai limpar o arquivo pra adicionar o que está no fprintf
     
+    if(file == NULL){
+        printf("Não existe tal arquivo");
+        getchar();
+        exit(0);
+    }
+    
+    char frase[100], palavras[100],*token;
 
+    // Lendo strings completas
+    //while(fgets(frase, 100, file) != NULL){
+    //    printf("%s", frase);
+    //}
+
+    // Lendo palavras da string -> Pensar numa maneira de separar as palavras
+    while(fgets(frase, 100, file) != NULL){
+        strcpy(frase, palavras);
+        for(int i = 0; palavras[i] != '\n'; i++){
+            printf("%c", palavras[i]);
+            if(palavras[i] == ' ') printf("\n");
+        }
+        token = strtok(frase, " ");
+        printf("\n");
+        printf("%s\n", token);
+        num++;
+    }
+    fclose(file);
+    printf("Nessa arquivo ha %d palavras\n", num);
     return 0;
 }
