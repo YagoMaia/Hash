0ff374f2a5ef27e54a5698585e77cdd6feac84da
YagoMaia <yagohenriquev123@gmail.com>
2023-05-30 16:11:46 -0300

    modified:   manipulacao.c 	modified:   output/manipulacao.exe 	modified:   output/teste.txt

--- manipulacao.c
+++ manipulacao.c
@@ -1,35 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAM_1 30
+#define TAM_2 100
+#define fim_palavras 12
 
-int main(void){
+int leitor_linhas(char texto[TAM_2], FILE *arquivo, char separadas[TAM_1])
+{
+    char black_list[fim_palavras] = {' ', '\n', '.', ',', '/', '-', ';', '?', '!', '*', ':', '"'};
+    int k, numero_palavras;
+    k = numero_palavras = 0;
+    while (fgets(texto, TAM_2, arquivo) != NULL)
+    {
+        for (unsigned int i = 0; i < strlen(texto); i++, k++)
+        {   
+            for (int cases = 0; cases < fim_palavras; cases++)
+            {
+                if(texto[i] != black_list[cases]){
+                    separadas[k] = texto[i];
+                }
+                else if (texto[i] == black_list[cases])
+                {
+                    if(k!= 0) numero_palavras++; //Gambiarra 
+                    separadas[k] = '\0'; 
+                    printf("Valor do k: %d ", k);
+                    puts(separadas);
+                    k = -1;
+                }
+            }
+            
+        }
+        printf("Fim da linha\n");
+    }
+    return numero_palavras;
+}
+
+int main(void)
+{
     int num = 0;
     FILE *file;
-    file = fopen("brascubas.txt", "r");
+    file = fopen("teste.txt", "r");
     // fprintf(file, "Algo depois do hello world."); //Vai limpar o arquivo pra adicionar o que está no fprintf
-    
-    if(file == NULL){
+
+    if (file == NULL)
+    {
         printf("Não existe tal arquivo");
         getchar();
         exit(0);
     }
-    
-    char frase[100];
 
-    // Lendo strings completas
-    //while(fgets(frase, 100, file) != NULL){
-    //    printf("%s", frase);
-    //}
+    char frase[TAM_2], palavra[TAM_1];
 
-    // Lendo palavras da string -> Pensar numa maneira de separar as palavras
-    while(fgets(frase, 100, file) != NULL){
-        for(int i = 0; i < strlen(frase); i++){
-            if(frase[i] == ' ' || frase[i] == '\n'){
-                if(frase[i] != '\n') num++;
-            }
-        }
-        //printf("%s", frase);
-    }
+    num = leitor_linhas(frase, file, palavra);
     fclose(file);
     printf("Nessa arquivo ha %d palavras\n", num);
     return 0;
