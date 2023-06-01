79f5966b8884f2645c44482eeb62a8f9c8e3fee8
YagoMaia <yagohenriquev123@gmail.com>
2023-06-01 17:57:51 -0300

    new file:   output/prep.exe 	modified:   output/strtok.exe 	modified:   strtok.c

--- strtok.c
+++ strtok.c
@@ -3,36 +3,82 @@
 #include <string.h>
 #define TAM_1 46
 #define TAM_2 100
-#define TABELA 80000
-#define fim_palavras 12
+#define TABELA 50
+#define prepos 39
+#define max_tam_prep 8
 
-int leitor_linhas(char texto[TAM_2], FILE *arquivo)
+typedef struct dados{
+    char *palavra;
+    int cont;
+}Dados;
+
+int hashcode(char *s)
+{
+    int r = 0, i;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        r = (r << 5) | (r >> 27);
+        r = (int)s[i] ^ r;
+    }
+    return r;
+}
+
+int MAD(int  hash){
+    int multiplica = 8;
+    int soma = 11;
+    int divisor_primo = 23;
+    return (((multiplica * hash + soma)%divisor_primo)%TABELA);
+}
+
+void analise_tabela(Dados tabela[], int posicao, char * palavra_texto){
+    if(posicao < 0) posicao = -posicao;
+    if(tabela[posicao].cont == 0){
+        strcpy(tabela[posicao].palavra, palavra_texto);
+        tabela[posicao].cont = 1;
+    }
+    else tabela[posicao].cont++;
+}
+
+int leitor_linhas(char texto[TAM_2], FILE *arquivo, Dados tabela_hash[TABELA])
 {
     char *sub_string;
     char black_list[TAM_1] = " ,.!?\n\r#@-_+=/;*:1234567890";
-    char matriz_prep[10][30] = {{"de"}, {"das"}, {"o"}, {"dos"},{"os"},{"as"},{"em"}};
-    //matriz_palavras[quantidade_palavras -> Seria o tamanho da listas hash][tamanho_maior_palavra -> 46 letras -> TAM_1];
-    int numero_palavras;
-    numero_palavras = 0;
+    char matriz_prep[prepos][max_tam_prep] = {{"pelo"}, {"ao"}, {"pro"}, {"do"}, {"no"}, {"pela"}, {"a"}, {"pra"}, {"da"}, {"na"}, {"pelos"}, {"aos"}, {"pros"}, {"dos"}, {"nos"}, {"pelas"}, {"as"}, {"pras"}, {"das"}, {"nas"}, {"num"}, {"numa"}, {"numas"}, {"nele"}, {"neles"}, {"nelas"}, {"nelas"}, {"neste"}, {"nisto"}, {"nesse"}, {"nesses"}, {"nisso"}, {"aquele"}, {"aquela"}, {"daquilo"}, {"daquele"}, {"naquele"}, {"naquilo"}, {"de"}};
+    int numero_palavras = 0;
+    int pos_tabela;
     while (fgets(texto, TAM_2, arquivo) != NULL)
     {
         sub_string = strtok(texto, black_list);
         while(sub_string != NULL){
-            puts(sub_string);
-            for(int i = 0; i < 10; i++){
-              if(strcmp(sub_string, matriz_prep[i]) == 0){
-                numero_palavras--;
-                }
+            for(int i = 0; i < prepos; i++){
+              if(strcmp(sub_string, matriz_prep[i]) == 0) sub_string = strtok(NULL, black_list);
             }
-            sub_string = strtok(NULL, black_list);
-            numero_palavras++;
+            if(sub_string != NULL){
+                puts(sub_string);
+                pos_tabela = MAD(hashcode(sub_string));
+               //analise_tabela(tabela_hash, pos_tabela, sub_string);
+                printf("Posicao tabela = %d\n", pos_tabela);
+                sub_string = strtok(NULL, black_list);
+                numero_palavras++;
+            };
         }
     }
     return numero_palavras;
 }
 
+void conteudo_tabela(Dados *tabela){
+    for(int i = 0; i < TABELA; i++){
+        printf("Vezes %d\n",tabela[i].cont);
+        puts(tabela[i].palavra);
+    }
+}
+
 int main(void)
 {
+    Dados tabela_hash[TABELA];
+    for(int i = 0; i < TABELA; i++){
+        tabela_hash[i].cont = 0;
+    }
     int num = 0;
     FILE *file;
     file = fopen("teste.txt", "r");
@@ -46,8 +92,9 @@ int main(void)
 
     char frase[TAM_2];
 
-    num = leitor_linhas(frase, file);
+    num = leitor_linhas(frase, file, tabela_hash);
     fclose(file);
+    //conteudo_tabela(tabela_hash);
     printf("Nessa arquivo ha %d palavras\n", num);
     return 0;
 }
