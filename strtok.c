538725cb8ffaf6ba1a0b171d79761069f6129492
Yago Maia <79180855+YagoMaia@users.noreply.github.com>
2023-05-31 22:01:50 -0300

    Update strtok.c

--- strtok.c
+++ strtok.c
@@ -3,27 +3,14 @@
 #include <string.h>
 #define TAM_1 46
 #define TAM_2 100
+#define TABELA 80000
 #define fim_palavras 12
-
-char soma(char i, char j){
-    
-}
-
-int metodo_dobra(char *palavra, int tam_dobra, int tot_dig){
-    char *temp;
-    strcpy(palavra, temp);
-    for(int i = 0; i < (tot_dig/tam_dobra)-1; ++i){
-        for(int j=0; j < tam_dobra; ++j){
-            temp[i*tam_dobra+j+tam_dobra] = soma(temp[i*tam_dobra+j+tam_dobra], temp[(i*tam_dobra+j+tam_dobra) - (2*j + 1)]);
-        }
-    }
-    return (int)temp;
-}
 
 int leitor_linhas(char texto[TAM_2], FILE *arquivo)
 {
     char *sub_string;
-    char black_list[TAM_1] = " ,.!?\n#@-_+=/;*:1234567890";
+    char black_list[TAM_1] = " ,.!?\n\r#@-_+=/;*:1234567890";
+    char matriz_prep[10][30] = {{"de"}, {"das"}, {"o"}, {"dos"},{"os"},{"as"},{"em"}};
     //matriz_palavras[quantidade_palavras -> Seria o tamanho da listas hash][tamanho_maior_palavra -> 46 letras -> TAM_1];
     int numero_palavras;
     numero_palavras = 0;
@@ -31,7 +18,12 @@ int leitor_linhas(char texto[TAM_2], FILE *arquivo)
     {
         sub_string = strtok(texto, black_list);
         while(sub_string != NULL){
-            //puts(sub_string);
+            puts(sub_string);
+            for(int i = 0; i < 10; i++){
+              if(strcmp(sub_string, matriz_prep[i]) == 0){
+                numero_palavras--;
+                }
+            }
             sub_string = strtok(NULL, black_list);
             numero_palavras++;
         }
@@ -43,7 +35,7 @@ int main(void)
 {
     int num = 0;
     FILE *file;
-    file = fopen("brascubas.txt", "r");
+    file = fopen("teste.txt", "r");
 
     if (file == NULL)
     {
